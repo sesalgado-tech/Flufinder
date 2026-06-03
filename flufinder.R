2f158277dc337fbb681ed9d8dee7b58b4d171fc3
Sofia Salgado <sesalgado@ucsd.edu>
2026-06-03 11:12:03 -0700

    I added functions 1, 3, and 5.

--- flufinder.R
+++ flufinder.R
@@ -1,3 +1,19 @@
+## Function 1: upload_fasta()
+
+upload_fasta <- function(testfasta.txt) {
+  #Opening seqinr library for handling FASTA files; make sure you have seqinr installed
+  library(seqinr)
+  
+  #Reading the fasta file
+  read.fasta(testfasta.txt, seqtype = "AA", as.string = TRUE,
+             set.attributes = FALSE)
+}
+
+upload_fasta("testfasta.txt")
+
+upload_fasta("testfasta.txt")$A
+
+
 #2:
 trypsinize <- function(proteins) {
   
@@ -10,6 +26,26 @@ trypsinize <- function(proteins) {
   # peptide bond on the C-terminal side of R and K.
   lapply(proteins, str_split_1, pattern="(?<=R|K)")
 }
+
+
+## Function 3: split_peptides()
+
+split_peptides <- function(peptides) {
+  # Opening stringr for simple string manipulation
+  library(stringr)
+  #Splitting peptides into individual amino acids using str_split; generates a list of lists of amino acids for each peptide
+  lapply(peptides, str_split, pattern="")
+}
+
+peptides <- list(A=c("LVK","LHHIIFESMLK","DMQR","R","HR","VW"),
+                 B=c("ADEFQGSMQK","IEACWQSYDVQF"),
+                 C=c("MINEPFSWR","LEFHLSER","K","YDEIM"))
+
+split_peptides(peptides)
+
+split_peptides(peptides)$B
+
+
 #4:
 splitpeptides_to_masses <- function(aa) {
   
@@ -33,6 +69,39 @@ splitpeptides_to_masses <- function(aa) {
   # Unlist the inner lists so each protein has a simple numeric vector
   lapply(peptide_masses, unlist)
 }
+
+
+## Function 5: count_matching_masses()
+
+count_matching_masses <- function(protein_masses, sample, masses_list)
+{
+  #Virus masses is a list of masses for each protein so we use sapply to iterate over the list; sum (of TRUEs) is used to count the number of times a mass in the sample is found (%in%) among the masses of each of the proteins (virus_masses); note that masses are converted into strings (as.character) because %in% is not very reliable with numbers
+  
+  df <- as.data.frame(sapply(protein_masses, function (x)
+    sum(as.character(sample) %in% as.character(x))))
+  # Adding peptide_counts as the column name of the counts column
+  
+  names(df) <- "peptide_counts"
+  return(df) 
+}
+
+masses_list <- list(
+  A = c(340.246, 1348.728, 530.225),
+  B = c(1121.476, 1469.624),
+  C = c(1160.540, 1011.511, 651.255)
+)
+
+sample <- c(340.246, 530.225, 1348.728)
+
+count_matching_masses(masses_list, sample)
+
+# Create a new sample vector (randomly picked from masses_list)
+new_sample <- c(340.246, 1121.476, 651.255)
+
+# Run the function
+count_matching_masses(masses_list, new_sample)
+
+
 #6:
 ggbarplot <- function(peptide_counts_table) {
   
